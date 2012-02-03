4203bb6e24864a40b74b14c8c837060efa361f37
Miguel Huerta <mikehf7@gmail.com>
2012-02-03 14:01:41 -0500

    data fixtures agregaddas

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Symfony\Bundle\DoctrineFixturesBundle\DoctrineFixturesBundle(),
             new Noticias\AdministradorBundle\AdministradorBundle(),
             new Noticias\DiarioBundle\DiarioBundle(),
             new Noticias\EstadisticaBundle\EstadisticaBundle(),
