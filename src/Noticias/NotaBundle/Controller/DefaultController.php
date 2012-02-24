d929b5bed181b69e52866acf53686e2600028529
art jimenez <art_bsaa@hotmail.com>
2012-02-24 14:01:17 -0600

    form ultimo

--- src/Noticias/NotaBundle/Controller/DefaultController.php
+++ src/Noticias/NotaBundle/Controller/DefaultController.php
@@ -3,6 +3,9 @@
 namespace Noticias\NotaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Noticias\NotaBundle\Entity\Nota;
+
+use Noticias\NotaBundle\Form\NotaType;
 
 
 class DefaultController extends Controller
