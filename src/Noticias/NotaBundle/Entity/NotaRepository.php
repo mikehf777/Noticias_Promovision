91278cb9e6133d85175bce6e3a9bda81c091a4d4
Jorge Pool <xPoolitoX@gmail.com>
2012-03-03 10:51:43 -0600

    change notarepository.php

--- src/Noticias/NotaBundle/Entity/NotaRepository.php
+++ src/Noticias/NotaBundle/Entity/NotaRepository.php
@@ -6,29 +6,19 @@ class NotaRepository extends EntityRepository
 {
     public function findAvancesDelDia_Reporteros($usuario)
     {
-        $hoy = new \DateTime('today', 'YYYY/DD/MM hh:mm:ss');
-        $hoy->format('Y/m/d');
+        $hoy = \date('Y-m-d');
         $em = $this->getEntityManager();
-        $dql='SELECT n
+        $dql='SELECT n, u
                FROM NotaBundle:Nota n
                JOIN n.usuario u
-<<<<<<< HEAD
-               WHERE n.fecha_crea >= :fecha
-                 AND n.usuario = :usuario
-              ';
-        $consulta = $em->createQuery($dql);
-        $consulta->setParameter('fecha', '01XX-XX-XX 00:00:00');
-=======
                WHERE n.fecha_crea >= :fecha 
                  AND n.usuario = :usuario
               ORDER BY n.fecha_crea ASC';
         $consulta = $em->createQuery($dql);
-        $consulta->setParameter('fecha', $hoy->format('d/m/Y').'00:00:00');
->>>>>>> f34b26abee5f14772ed54a52bbd8e71832d7102f
+        $consulta->setParameter('fecha', $hoy.'00:00:00');
         $consulta->setParameter('usuario',$usuario);
         
-        
-        return $consulta->getResult();    
+        return $consulta->getResult();
     }
 }
 ?>
