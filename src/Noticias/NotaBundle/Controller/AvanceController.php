286b4468356a5cb4e7c4b88f4df4675304837372
art jimenez <art_bsaa@hotmail.com>
2012-03-12 10:13:28 -0600

    form editar,ver

--- src/Noticias/NotaBundle/Controller/AvanceController.php
+++ src/Noticias/NotaBundle/Controller/AvanceController.php
@@ -7,6 +7,7 @@ use Noticias\NotaBundle\Entity\Nota;
 use Noticias\NotaBundle\Entity\Fuente;
 
 use Noticias\NotaBundle\Form\AvanceType;
+use Noticias\NotaBundle\Form\EditarAvanceType;
 
 
 class AvanceController extends Controller
@@ -19,7 +20,7 @@ class AvanceController extends Controller
         $avances=$em->getRepository('NotaBundle:Nota')->findNotasdeldia_Usuario($usuario); //////Pasar id del usuario logeado
         return $this->render('NotaBundle:Default:tabla_avance.html.twig',array('avances'=>$avances));    
     }
-    //Action para crrar la tabla de avances para jefes 
+    //Action para crear la tabla de avances para jefes 
     public function avances_jefesAction()
     {
      $usuario = $this->get('security.context')->getToken()->getUser();
@@ -29,10 +30,20 @@ class AvanceController extends Controller
     }
     public function opcionesavancesAction($opcion)
     {
+        
+        $nota = new Nota();   
+        $formulario = $this->createForm(new EditarAvanceType(), $nota);
+        
         $em=$this->getDoctrine()->getEntityManager();
+        $usercams = $em->getRepository('UsuarioBundle:Usuario')->findBy(array(
+                                       'puesto' => 'Camarografo'));
+        $fuentes = $em->getRepository('NotaBundle:Fuente')->findAll();
         $peticion=$this->getRequest();
-        $nota=$peticion->request->get('nota');
-        //$avance=$em->getRepository('NotaBundle:Nota')->findOneBy(array('id' =>$nota_id));
-        return $this->render('NotaBundle:Default:'.$opcion.'_avance.html.twig',array('avance'=>$nota));   
+        $nota_id=$peticion->request->get("id");
+        $avance=$em->getRepository('NotaBundle:Nota')->findOneBy(array('id' => $nota_id ));
+        return $this->render('NotaBundle:Default:'.$opcion.'_avance.html.twig',array('avance'=>$avance,
+                                                                    'usercams'=>$usercams,
+                                                                    'fuentes'=>$fuentes,
+                                                                    'formulario' => $formulario->createView()));   
     }        
 }
